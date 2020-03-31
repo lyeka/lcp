5487b8262481f71711033c70bacf95c9bc5da545
lumingheng <lumingheng@youmi.net>
2020-03-31 19:51:00 +0800

    add 21

---
+++ easy/21_test.go
@@ -0,0 +1,14 @@
+package easy
+
+import (
+	"testing"
+)
+
+func TestMergeTwoLists2(t *testing.T)  {
+	l1 := &ListNode{Val:1}
+	l2 := &ListNode{Val:2, Next:&ListNode{Val:3}}
+
+	mergeTwoListsWrong(l1, l2)
+	mergeTwoLists(l1, l2)
+	mergeTwoListsOptimization(l1, l2)
+}
