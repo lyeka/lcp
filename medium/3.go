2da29af45abbe9a2b14f30d8b84037807684cb53
lumingheng <lumingheng@youmi.net>
2020-04-03 14:06:07 +0800

    优化3

--- medium/3.go
+++ medium/3.go
@@ -4,10 +4,7 @@ import "strings"
 
 // 滑动窗口
 func LengthOfLongestSubstring(s string) int {
-	if s == "" {
-		return 0
-	}
-	i, j, max := 0, 1, 1
+	i, j, max := 0, 0, 0
 	l := len(s)
 	for i < l && j < l {
 		if !strings.Contains(s[i:j], string(s[j])) {
@@ -16,6 +13,49 @@ func LengthOfLongestSubstring(s string) int {
 			}
 			j++
 		} else {
+			i++
+		}
+	}
+
+	return max
+}
+
+
+// 使用map来优化，但是速度更慢， 内存更大，应该是删除map元素导致的
+func LengthOfLongestSubstring2(s string) int {
+	i, j, max := 0, 0, 0
+	l := len(s)
+	set := make(map[byte]int, l)
+	for i < l && j < l {
+		if _, ok := set[s[j]]; !ok{
+			if n := j-i+1; n > max {
+				max = n
+			}
+			set[s[j]] = 1
+			j++
+		} else {
+			delete(set, s[i])
+			i++
+		}
+	}
+
+	return max
+}
+
+// 使用map来优化， 不删除元素， 速度提升，内存变大
+func LengthOfLongestSubstring3(s string) int {
+	i, j, max := 0, 0, 0
+	l := len(s)
+	set := make(map[byte]bool, l)
+	for i < l && j < l {
+		if exist := set[s[j]]; !exist {
+			if n := j-i+1; n > max {
+				max = n
+			}
+			set[s[j]] = true
+			j++
+		} else {
+			set[s[i]]=false
 			i++
 		}
 	}
--- medium/3_test.go
+++ medium/3_test.go
@@ -2,8 +2,48 @@ package medium
 
 import "testing"
 
+const (
+	s1 = "pwwkew"
+	s2 = "abcabcbb"
+	s3 = ""
+)
+
 func TestLengthOfLongestSubstring(t *testing.T) {
-	if LengthOfLongestSubstring("pwwkew") != 3 {
-		t.Fail()
+
+	if LengthOfLongestSubstring(s1) != 3 {
+		t.Fatal(s1)
+	}
+
+	if LengthOfLongestSubstring(s3) != 0 {
+		t.Fatal(s3)
+	}
+
+}
+
+func TestLengthOfLongestSubstring2(t *testing.T) {
+	if LengthOfLongestSubstring2(s1) != 3 {
+		t.Fatal(s1)
+	}
+
+	if LengthOfLongestSubstring2(s2) != 3 {
+		t.Fatal(s2)
+	}
+
+	if LengthOfLongestSubstring2(s3) != 0 {
+		t.Fatal(s3)
+	}
+}
+
+func TestLengthOfLongestSubstring3(t *testing.T) {
+	if LengthOfLongestSubstring3(s1) != 3 {
+		t.Fatal(s1)
+	}
+
+	if LengthOfLongestSubstring3(s2) != 3 {
+		t.Fatal(s2)
+	}
+
+	if LengthOfLongestSubstring3(s3) != 0 {
+		t.Fatal(s3)
 	}
 }
